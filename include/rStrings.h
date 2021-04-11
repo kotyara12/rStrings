b7f104218325658691566453495122fad651184f
kotyara12 <kotyara12@users.noreply.github.com>
2021-04-11 23:58:05 +0300

    Added functions for generating MQTT topics according to certain rules

--- include/rStrings.h
+++ include/rStrings.h
@@ -15,9 +15,83 @@
 extern "C" {
 #endif
 
+/**
+ * Clone a string and allocate a new memory area on the heap
+ * */
 char* malloc_string(const char *source);
+
+/**
+ * Generating a string in heap with formatting
+ * */
 char* malloc_stringf(const char *format, ...);
+
+/**
+ * Generating a heap string containing a textual representation of the date and time
+ * */
 char* malloc_timestr(const char *format, time_t value);
+
+/**
+ * Generation of a truncated name of a topic: prefix + site + / + topic 
+ * for example: "/home/heater"
+ * 
+ * Note: mqttGetTopic0 uses values from project_congig.h
+ * 
+ * @param perfix - Server topic prefix, may be "/" or "" or "user_XXXX" for some servers
+ * @param site - Device location, may be "home" or "village"
+ * @param topic - Topic name
+ * @return - Pointer to a string in heap. Remember to free it after using the function esp_mqtt_free_string() or free();
+ * */
+char * mqttGetTopicCustom0(const char *perfix, const char *site, const char *topic);
+char * mqttGetTopic0(const char *topic);
+
+/**
+ * Generation of a name of a topic: prefix + site + / + device + / + topic
+ * for example: "/village/heater/status"
+ * 
+ * Note: mqttGetTopic1 uses values from project_congig.h
+ * 
+ * @param perfix - Server topic prefix, may be "/" or "" or "user_XXXX" for some servers
+ * @param site - Device location, may be "home" or "village"
+ * @param device - Esp device name 
+ * @param topic - Topic name
+ * @return - Pointer to a string in heap. Remember to free it after using the function esp_mqtt_free_string() or free();
+ * */
+char * mqttGetTopicCustom1(const char *perfix, const char *site, const char *device, const char *topic);
+char * mqttGetTopic1(const char *topic);
+
+/**
+ * Generation of a name of a topic: prefix + site + / + device + / + topic1 + / + topic2
+ * for example: "/village/heater/bedroom/temperature"
+ * 
+ * Note: mqttGetTopic1 uses values from project_congig.h
+ * 
+ * @param perfix - Server topic prefix, may be "/" or "" or "user_XXXX" for some servers
+ * @param site - Device location, may be "home" or "village"
+ * @param device - Esp device name 
+ * @param topic1 - Topic name
+ * @param topic2 - Subtopic name
+ * @return - Pointer to a string in heap. Remember to free it after using the function esp_mqtt_free_string() or free();
+ * */
+char * mqttGetTopicCustom2(const char *perfix, const char *site, const char *device, const char *topic1, const char *topic2);
+char * mqttGetTopic2(const char *topic1, const char *topic2);
+
+/**
+ * Generation of a name of a topic: prefix + site + / + device + / + topic1 + / + topic2 + / + topic3
+ * for example: "/village/heater/bedroom/temperature/sensor_status"
+ * 
+ * Note: mqttGetTopic1 uses values from project_congig.h
+ * 
+ * @param perfix - Server topic prefix, may be "/" or "" or "user_XXXX" for some servers
+ * @param site - Device location, may be "home" or "village"
+ * @param device - Ssp device name 
+ * @param topic1 - Topic name
+ * @param topic2 - Subtopic name
+ * @param topic3 - Subsubtopic name
+ * @return - Pointer to a string in heap. Remember to free it after using the function esp_mqtt_free_string() or free();
+ * */
+char * mqttGetTopicCustom3(const char *perfix, const char *site, const char *device, const char *topic1, const char *topic2, const char *topic3);
+char * mqttGetTopic3(const char *topic1, const char *topic2, const char *topic3);
+char * mqttGetTopic(const char *topic1, const char *topic2, const char *topic3);
 
 #ifdef __cplusplus
 }
--- src/rStrings.cpp
+++ src/rStrings.cpp
@@ -7,6 +7,10 @@
 
 static const char * tagHEAP = "OUT OF MEMORY";
 
+// -----------------------------------------------------------------------------------------------------------------------
+// -------------------------------------------------- Format strings -----------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------------------
+
 char * malloc_string(const char *source) 
 {
   uint32_t len = strlen(source);
@@ -50,5 +54,86 @@ char * malloc_timestr(const char *format, time_t value)
   localtime_r(&value, &timeinfo);
   strftime(buffer, sizeof(buffer), format, &timeinfo);
   return malloc_string(buffer);
+}
+
+// -----------------------------------------------------------------------------------------------------------------------
+// -------------------------------------------------- Create topics ------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------------------
+
+#define MQTT_TOPIC_TEMPLATE_0 "%s%s/%s"          // prefix + site + / + topic (etc: "/village/heater")
+#define MQTT_TOPIC_TEMPLATE_1 "%s%s/%s/%s"       // prefix + site + / + device + / + topic (etc: "/village/heater/status")
+#define MQTT_TOPIC_TEMPLATE_2 "%s%s/%s/%s/%s"    // prefix + site + / + device + / + topic + / + subtopic (etc: "/village/heater/bedroom/temperature")
+#define MQTT_TOPIC_TEMPLATE_3 "%s%s/%s/%s/%s/%s" // prefix + site + / + device + / + topic1 + / + topic2 + / + topic3 (etc: "/village/heater/bedroom/temperature/sensor_status")
+
+char * mqttGetTopicCustom0(const char *perfix, const char *site, const char *topic)
+{
+  return malloc_stringf(MQTT_TOPIC_TEMPLATE_0, perfix, site, topic);
+}
+
+char * mqttGetTopic0(const char *topic)
+{
+  #ifdef CONFIG_MQTT_PREFIX
+    return malloc_stringf(MQTT_TOPIC_TEMPLATE_0, CONFIG_MQTT_PREFIX, CONFIG_MQTT_SITE, topic);
+  #else
+    return malloc_stringf(MQTT_TOPIC_TEMPLATE_0, "", CONFIG_MQTT_SITE, topic);
+  #endif
+}
+
+char * mqttGetTopicCustom1(const char *perfix, const char *site, const char *device, const char *topic)
+{
+  return malloc_stringf(MQTT_TOPIC_TEMPLATE_1, perfix, site, device, topic);
+}
+
+char * mqttGetTopic1(const char *topic)
+{
+  #ifdef CONFIG_MQTT_PREFIX
+    return malloc_stringf(MQTT_TOPIC_TEMPLATE_1, CONFIG_MQTT_PREFIX, CONFIG_MQTT_SITE, CONFIG_MQTT_DEVICE, topic);
+  #else
+    return malloc_stringf(MQTT_TOPIC_TEMPLATE_1, "", CONFIG_MQTT_PREFIX, CONFIG_MQTT_SITE, CONFIG_MQTT_DEVICE, topic);
+  #endif
+}
+
+char * mqttGetTopicCustom2(const char *perfix, const char *site, const char *device, const char *topic1, const char *topic2)
+{
+  return malloc_stringf(MQTT_TOPIC_TEMPLATE_2, perfix, site, device, topic1, topic2);
+}
+
+char * mqttGetTopic2(const char *topic1, const char *topic2)
+{
+  #ifdef CONFIG_MQTT_PREFIX
+    return malloc_stringf(MQTT_TOPIC_TEMPLATE_2, CONFIG_MQTT_PREFIX, CONFIG_MQTT_SITE, CONFIG_MQTT_DEVICE, topic1, topic2);
+  #else
+    return malloc_stringf(MQTT_TOPIC_TEMPLATE_2, "", CONFIG_MQTT_PREFIX, CONFIG_MQTT_SITE, CONFIG_MQTT_DEVICE, topic1, topic2);
+  #endif
+}
+
+char * mqttGetTopicCustom3(const char *perfix, const char *site, const char *device, const char *topic1, const char *topic2, const char *topic3)
+{
+  return malloc_stringf(MQTT_TOPIC_TEMPLATE_3, perfix, site, device, topic1, topic2, topic3);
+}
+
+char * mqttGetTopic3(const char *topic1, const char *topic2, const char *topic3)
+{
+  #ifdef CONFIG_MQTT_PREFIX
+    return malloc_stringf(MQTT_TOPIC_TEMPLATE_3, CONFIG_MQTT_PREFIX, CONFIG_MQTT_SITE, CONFIG_MQTT_DEVICE, topic1, topic2, topic3);
+  #else
+    return malloc_stringf(MQTT_TOPIC_TEMPLATE_3, "", CONFIG_MQTT_SITE, CONFIG_MQTT_DEVICE, topic1, topic2, topic3);
+  #endif
+}
+
+char * mqttGetTopic(const char *topic1, const char *topic2, const char *topic3)
+{
+  if (topic3) {
+    return mqttGetTopic3(topic1, topic2, topic3);
+  }
+  else if (topic2) {
+    return mqttGetTopic2(topic1, topic2);
+  }
+  else if (topic1) {
+    return mqttGetTopic1(topic1);
+  }
+  else {
+    return mqttGetTopic0(CONFIG_MQTT_DEVICE);
+  }
 }
 
